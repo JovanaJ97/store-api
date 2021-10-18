2bf8581bbb52060429f85c0839d3c618f8fbe6ef
Jovana Jovanovic <jovana@timacum.com>
2021-10-18 09:25:44 +0200

    new functionalities

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Selectors
 const productContainer = document.querySelector('.product-container');
 const customerContainer = document.querySelector('.users-container');
 const stickyHeader = document.querySelector('header');
@@ -6,7 +7,25 @@ const closeBtn = document.querySelector('.fa-times');
 const navList = document.querySelector('ul');
 const menuBtn = document.querySelector('.mobile-nav');
 const topBtn = document.querySelector('.top');
+const question = document.querySelectorAll('.question')
+const toggle = document.querySelector('.fa-lightbulb');
+const tabs = document.querySelectorAll('.tab-single')
 
+// Dark theme switcher
+toggle.addEventListener('click',() => {
+    document.body.classList.toggle('dark-theme')
+
+    if(document.body.classList.contains('dark-theme')) {
+        toggle.classList.add('far');
+        toggle.classList.remove('fas');
+    } else {
+        toggle.classList.remove('far');
+        toggle.classList.add('fas');
+    }
+
+})
+
+// Functions for mobile navigation
 const openMenu = () => {
     barsBtn.style.display = 'none';
     closeBtn.style.display = 'block';
@@ -17,6 +36,7 @@ const closeMenu = () => {
     closeBtn.style.display = 'none';
 }
 
+// Mobile nav
 menuBtn.addEventListener('click', () => {
     navList.classList.toggle('list');
 
@@ -28,7 +48,7 @@ menuBtn.addEventListener('click', () => {
 
 })
 
-
+//Sticky header and scroll to top
 window.addEventListener('scroll', () => {
     const scrollHeight = window.pageYOffset;
     const headerHeight = stickyHeader.offsetHeight;
@@ -51,7 +71,7 @@ topBtn.addEventListener('click', () => {
     document.documentElement.scrollTop = 0;
 })
 
-
+// Fetching products
 async function getProducts() {
     const results = await fetch('https://fakestoreapi.com/products');
     const data = await results.json();
@@ -64,8 +84,9 @@ async function getProducts() {
             <img src="${product.image}" alt="${product.title}">
         </div>
         <div class="product-info">
-            <h4 class="price">${product.title}</h4>
+            <h3>${product.title}</h3>
             <p class="description">${product.description}</p>
+            <h4 class="price">${product.price}$</h4>
             <div class="overlay">
                 <h3>Product ID: ${product.id}</h3>
                 <p>Category: ${product.category}</p>
@@ -79,6 +100,7 @@ async function getProducts() {
     })
 }
 
+// Fetching customers
 async function getCustomers() {
     const resCustomers = await fetch('https://randomuser.me/api/?results=5');
     const { results } = await resCustomers.json();
@@ -88,13 +110,53 @@ async function getCustomers() {
         customerEl.classList.add('customer');
         customerEl.innerHTML = `
         <img src=${customer.picture.medium} alt=${customer.name.first} class="customer-img"/>
-        <h4>${customer.name.first} ${customer.name.last}</h4>
-        <p>${customer.email}</p>
+        <h3>${customer.name.first} ${customer.name.last}</h3>
+        <p>${customer.location.country}</p>
+        <div class="customer-info">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+            </p>
+        </div>
         `
         
         customerContainer.appendChild(customerEl)
     })
 }
+
+// FAQ
+let oldEl = null;
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        let currentTab = e.currentTarget;
+
+        if(oldEl !== null & oldEl !== currentTab) {
+            oldEl.classList.remove('show');
+
+            oldEl = tab;
+        }
+
+        const openTab = () => {
+            tab.classList.add('show')
+        }
+
+        const closeTab = () => {
+            tab.classList.remove('show')
+        }
+
+        if(tab.classList.contains('show')) {
+            closeTab();
+            
+            oldEl = null;
+
+        } else {
+            openTab()
+
+            oldEl = tab;
+        }
+    })
+})
+
 
 getProducts();
 getCustomers();
